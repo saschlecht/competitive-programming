055ce7fcf9bae3e7863c6a849900d98a93f49ade
saschlecht <116755221+saschlecht@users.noreply.github.com>
2024-02-14 16:54:06 -0500

    Add files via upload

---
+++ GCD_of_Two_Numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+int main()
+{
+    int x, y;
+    cin >> x;
+    cin >> y;
+    int answer = gcd(x, y);
+    cout << answer;
+    return 0;
+}
